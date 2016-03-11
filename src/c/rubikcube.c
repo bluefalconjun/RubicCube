30084866761edc81893eda1a9a7d8d7b5707add6
juxu <jun.xu@montage-tech.com>
2016-03-11 14:54:52 +0800

    update version 0.1
    1. change cube structure.
    2. add photo to clear pos.

--- src/c/rubikcube.c
+++ src/c/rubikcube.c
@@ -3,7 +3,7 @@
 #include "rubikcube.h"
 
 int main(void) {
-  RubikCube *CurCube = calloc(1, sizeof(RubikCube));
+  Cube *CurCube = calloc(1, sizeof(Cube));
 
   init_cube(CurCube);
 
@@ -15,9 +15,9 @@ int main(void) {
   return 0;
 }
 
-void init_cube(RubikCube *cube) {
+void init_cube(Cube *cube) {
   assert(cube != NULL);
-  memset(cube, 0, sizeof(RubikCube));
+  memset(cube, 0, sizeof(Cube));
 
   // use initcolor for fit index
   BLOCKCOLOR initcolor[6];
@@ -28,24 +28,19 @@ void init_cube(RubikCube *cube) {
   initcolor[F] = ORANGE;
   initcolor[B] = RED;
 
-  int i, k, l;
-  for (i = 0; i < 6; i++) {
-    cube->face[i].middle.center = initcolor[i];
-    for (k = 0; k < 2; k++) {
-      cube->face[i].middle.edge[k] = initcolor[i];
-      cube->face[i].updown[k].edge = initcolor[i];
-      for (l = 0; l < 2; l++) {
-        cube->face[i].updown[k].corner[l] = initcolor[i];
-      }
+  int i, j;
+  for (i = 0; i < FACENUM; i++) {
+    for (j = 0; j < BLOCKNUM; j++) {
+      cube->face[i].block[j] = initcolor[i];
     }
   }
   return;
 }
 
-void show_cube(RubikCube *cube) {
+void show_cube(Cube *cube) {
   assert(cube != NULL);
   int i;
-  for (i = 0; i < 6; i++) {
+  for (i = 0; i < FACENUM; i++) {
     showoneface(cube, i);
   }
   return;
@@ -53,138 +48,148 @@ void show_cube(RubikCube *cube) {
 
 void showfacename(BLOCKPOS faceidx) {
   switch (faceidx) {
-  case U:
-    printf("UP:\n");
-    break;
-  case D:
-    printf("DOWN:\n");
-    break;
-  case L:
-    printf("LEFT:\n");
-    break;
-  case R:
-    printf("RIGHT:\n");
-    break;
-  case F:
-    printf("FRONT:\n");
-    break;
-  case B:
-    printf("BACK:\n");
-    break;
-  default:
-    assert(false);
+    case U:
+      printf("UP:\n");
+      break;
+    case D:
+      printf("DOWN:\n");
+      break;
+    case L:
+      printf("LEFT:\n");
+      break;
+    case R:
+      printf("RIGHT:\n");
+      break;
+    case F:
+      printf("FRONT:\n");
+      break;
+    case B:
+      printf("BACK:\n");
+      break;
+    default:
+      assert(false);
   }
   return;
 }
 void showblockcolor(BLOCKCOLOR color) {
   switch (color) {
-  case WHITE:
-    printf("WHITE ");
-    break;
-  case YELLOW:
-    printf("YELLOW ");
-    break;
-  case GREEN:
-    printf("GREEN ");
-    break;
-  case BLUE:
-    printf("BLUE ");
-    break;
-  case RED:
-    printf("RED ");
-    break;
-  case ORANGE:
-    printf("ORANGE ");
-    break;
-  default:
-    assert(false);
+    case WHITE:
+      printf("WHITE ");
+      break;
+    case YELLOW:
+      printf("YELLOW ");
+      break;
+    case GREEN:
+      printf("GREEN ");
+      break;
+    case BLUE:
+      printf("BLUE ");
+      break;
+    case RED:
+      printf("RED ");
+      break;
+    case ORANGE:
+      printf("ORANGE ");
+      break;
+    default:
+      assert(false);
   }
   return;
 }
 
-void showoneface(RubikCube *cube, BLOCKPOS faceidx) {
+void showoneface(Cube *cube, BLOCKPOS faceidx) {
   showfacename(faceidx);
-  // show block in face from up to down, left to right.
-  showblockcolor(cube->face[faceidx].updown[U].corner[U]);
-  showblockcolor(cube->face[faceidx].updown[U].edge);
-  showblockcolor(cube->face[faceidx].updown[U].corner[D]);
-  printf("\n");
 
-  showblockcolor(cube->face[faceidx].middle.edge[U]);
-  showblockcolor(cube->face[faceidx].middle.center);
-  showblockcolor(cube->face[faceidx].middle.edge[D]);
-  printf("\n");
-
-  showblockcolor(cube->face[faceidx].updown[D].corner[U]);
-  showblockcolor(cube->face[faceidx].updown[D].edge);
-  showblockcolor(cube->face[faceidx].updown[D].corner[D]);
-  printf("\n");
+  int i, j;
+  CubeRow tmprow;
+  for (i = 0; i < BLOCKNUM / BLOCKPERROW; i++) {
+    getrowinface(&cube->face[faceidx], i, &tmprow);
+    for (j = 0; j < BLOCKPERROW; j++) {
+      showblockcolor(tmprow.row[j]);
+    }
+    printf("\n");
+  }
 }
 
-void UpClockwise(RubikCube *cube) {
-#if 0
-    // record current cornerblock[faceidx][L], edgeblock[faceidx]
-    // cornerblock[faceidx][R]
-    BLOCKCOLOR tmpcornerl, tmpedge, tmpcornerr;
-
-    // for upper colums  of face F/L/B/R
-    tmpcornerl = cube->cornerblock[F][U][L];
-    tmpedge = cube->edgeblock[F][U];
-    tmpcornerr = cube->cornerblock[F][U][R];
-
-    cube->cornerblock[F][U][L] = cube->cornerblock[R][U][L];
-    cube->edgeblock[F][U] = cube->edgeblock[R][U];
-    cube->cornerblock[F][U][R] = cube->cornerblock[R][U][R];
-
-    cube->cornerblock[R][U][L] = cube->cornerblock[B][U][L];
-    cube->edgeblock[R][U] = cube->edgeblock[B][U];
-    cube->cornerblock[R][U][R] = cube->cornerblock[B][U][R];
-
-    cube->cornerblock[B][U][L] = cube->cornerblock[L][U][L];
-    cube->edgeblock[B][U] = cube->edgeblock[L][U];
-    cube->cornerblock[B][U][R] = cube->cornerblock[L][U][R];
-
-    cube->cornerblock[L][U][L] = tmpcornerl;
-    cube->edgeblock[L][U] = tmpedge;
-    cube->cornerblock[L][U][R] = tmpcornerr;
-
-    // for colums of face U
-    tmpcornerl = cube->cornerblock[U][D][L];
-    tmpedge = cube->edgeblock[U][D];
-
-    cube->cornerblock[U][D][L] = cube->cornerblock[U][D][R];
-    cube->edgeblock[U][D] = cube->edgeblock[U][R];
-
-    cube->cornerblock[U][D][R] = cube->cornerblock[U][U][R];
-    cube->edgeblock[U][R] = cube->edgeblock[U][U];
-
-    cube->cornerblock[U][U][R] = cube->cornerblock[U][U][L];
-    cube->edgeblock[U][U] = cube->edgeblock[U][L];
-
-    cube->cornerblock[U][U][L] = tmpcornerl;
-    cube->edgeblock[U][L] = tmpedge;
-#endif
+void getrowinface(CubeFace *face, int rowidx, CubeRow *rowinface) {
+  assert(rowinface != NULL);
+  int i;
+  for (i = 0; i < BLOCKPERROW; i++) {
+    rowinface->row[i] = face->block[rowidx * BLOCKPERROW + i];
+  }
   return;
 }
 
-void UpInvert(RubikCube *cube) { return; }
+void getcolumninface(CubeFace *face, int columnidx, CubeColumn *columninface) {
+  assert(columninface != NULL);
+  int i;
+  for (i = 0; i < BLOCKPERROW; i++) {
+    columninface->column[i] = face->block[columnidx + BLOCKPERROW * i];
+  }
+  return;
+}
 
-void DownClockwise(RubikCube *cube) { return; }
+void setrowinface(CubeFace *face, int rowidx, CubeRow rowinface) {
+  int i;
+  for (i = 0; i < BLOCKPERROW; i++) {
+    face->block[rowidx * BLOCKPERROW + i] = rowinface.row[i];
+  }
 
-void DownInvert(RubikCube *cube) { return; }
+  return;
+}
 
-void LeftClockwise(RubikCube *cube) { return; }
+void setcolumninface(CubeFace *face, int columnidx, CubeColumn columninface) {
+  int i;
+  for (i = 0; i < BLOCKPERROW; i++) {
+    face->block[columnidx + BLOCKPERROW * i] = columninface.column[i];
+  }
+  return;
+}
 
-void LeftInvert(RubikCube *cube) { return; }
+void FaceMove(Cube *cube, BLOCKPOS faceidx, bool isclockwise) {
+  assert(cube != NULL);
+  faceblockmove(&cube->face[faceidx], isclockwise);
+  sideblockmove(cube, faceidx, isclockwise);
+  return;
+}
 
-void RightClockwise(RubikCube *cube) { return; }
+void faceblockmove(CubeFace *face, bool isclockwise) {
+  assert(face != NULL);
+  int i;
+  if (isclockwise) {
+    for (i = 0; i < BLOCKNUM; i++) {
+    }
+  } else {
+  }
+  return;
+}
 
-void RightInvert(RubikCube *cube) { return; }
+void sideblockmove(Cube *cube, BLOCKPOS faceidx, bool isclockwise) { return; }
 
-void FrontClockwise(RubikCube *cube) { return; }
+void UpClockwise(Cube *cube) { return; }
 
-void FrontInvert(RubikCube *cube) { return; }
+void UpInvert(Cube *cube) { return; }
 
-void BackClockwise(RubikCube *cube) { return; }
+void DownClockwise(Cube *cube) { return; }
 
-void BackInvert(RubikCube *cube) { return; }
+void DownInvert(Cube *cube) { return; }
+
+void LeftClockwise(Cube *cube) {
+  assert(cube != NULL);
+
+  return;
+}
+
+void LeftInvert(Cube *cube) { return; }
+
+void RightClockwise(Cube *cube) { return; }
+
+void RightInvert(Cube *cube) { return; }
+
+void FrontClockwise(Cube *cube) { return; }
+
+void FrontInvert(Cube *cube) { return; }
+
+void BackClockwise(Cube *cube) { return; }
+
+void BackInvert(Cube *cube) { return; }
--- src/c/rubikcube.h
+++ src/c/rubikcube.h
@@ -7,52 +7,61 @@
 #include <string.h>
 #include <assert.h>
 
+// 3 degree cube has 9 block in 1 face.
+#define BLOCKNUM 9
+#define FACENUM 6
+#define BLOCKPERROW 3
+
 // color of the block
 typedef enum { WHITE = 0, YELLOW, BLUE, GREEN, RED, ORANGE } BLOCKCOLOR;
 // position of the block: face to cube with UP/DOWN/LEFT/RIGHT/FRONT/BACK
 typedef enum { U = 0, D, L, R, F, B } BLOCKPOS;
+// x means turn left/right.  y for u/d and z for f/b
+typedef enum { X = 0, Y, Z } CUBECOORDINATE;
+
+// use fixed table to do sideblockmove
+BLOCKPOS kXcoordMove[4] = {F, U, B, D};
+BLOCKPOS kYcoordMove[4] = {F, R, B, L};
+BLOCKPOS kZcoordMove[4] = {U, R, D, L};
 
 // USlice means up/down rows in 1 cube face
-typedef struct {
-  BLOCKCOLOR edge;
-  BLOCKCOLOR corner[2];
-} USlice;
+typedef struct { BLOCKCOLOR row[BLOCKPERROW]; } CubeRow;
 
 // CSlice means middle row in 1 cube face
-typedef struct {
-  BLOCKCOLOR center;
-  BLOCKCOLOR edge[2];
-} CSlice;
+typedef struct { BLOCKCOLOR column[BLOCKPERROW]; } CubeColumn;
 
 // face means 1 face contains 2 USlice(up/down) and 1 CSlice
-typedef struct {
-  CSlice middle;
-  USlice updown[2];
-} CubeFace;
+typedef struct { BLOCKCOLOR block[BLOCKNUM]; } CubeFace;
 
 // cube is organized by 6 faces.
-typedef struct { CubeFace face[6]; } RubikCube;
+typedef struct { CubeFace face[FACENUM]; } Cube;
 
-void init_cube(RubikCube *cube);
-void show_cube(RubikCube *cube);
+void init_cube(Cube *cube);
+void show_cube(Cube *cube);
 
-void showoneface(RubikCube *cube, BLOCKPOS faceidx);
+void showoneface(Cube *cube, BLOCKPOS faceidx);
 void showfacename(BLOCKPOS faceidx);
 void showblockcolor(BLOCKCOLOR color);
 
-// move process always switch 90 dgree
-void UpClockwise(RubikCube *cube);
-void UpInvert(RubikCube *cube); // UpInvertClockWise Move
-void DownClockwise(RubikCube *cube);
-void DownInvert(RubikCube *cube); // UpInvertClockWise Move
-void LeftClockwise(RubikCube *cube);
-void LeftInvert(RubikCube *cube); // UpInvertClockWise Move
-void RightClockwise(RubikCube *cube);
-void RightInvert(RubikCube *cube); // UpInvertClockWise Move
-void FrontClockwise(RubikCube *cube);
-void FrontInvert(RubikCube *cube); // UpInvertClockWise Move
-void BackClockwise(RubikCube *cube);
-void BackInvert(RubikCube *cube); // UpInvertClockWise Move
+void getrowinface(CubeFace *face, int rowidx, CubeRow *rowinface);
+void getcolumninface(CubeFace *face, int columnidx, CubeColumn *columninface);
+void setrowinface(CubeFace *face, int rowidx, CubeRow rowinface);
+void setcolumninface(CubeFace *face, int columnidx, CubeColumn columninface);
 
-void SliceMove();
-void FaceMove();
+void FaceMove(Cube *cube, BLOCKPOS faceidx, bool isclockwise);
+void faceblockmove(CubeFace *face, bool isclockwise);
+void sideblockmove(Cube *cube, BLOCKPOS faceidx, bool isclockwise);
+
+// move process always switch 90 dgree
+void UpClockwise(Cube *cube);
+void UpInvert(Cube *cube); // UpInvertClockWise Move
+void DownClockwise(Cube *cube);
+void DownInvert(Cube *cube); // UpInvertClockWise Move
+void LeftClockwise(Cube *cube);
+void LeftInvert(Cube *cube); // UpInvertClockWise Move
+void RightClockwise(Cube *cube);
+void RightInvert(Cube *cube); // UpInvertClockWise Move
+void FrontClockwise(Cube *cube);
+void FrontInvert(Cube *cube); // UpInvertClockWise Move
+void BackClockwise(Cube *cube);
+void BackInvert(Cube *cube); // UpInvertClockWise Move
